916e4abb9bce781214d63e6530cdd4e944a8d9f9
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2021-08-11 01:56:36 -0300

    Adição dos inputs "wines" e "votes"

--- app/utils/acuracy.py
+++ app/utils/acuracy.py
@@ -1,15 +1,10 @@
+from sklearn import metrics
+
+
 class Acuracy:
   def __init__(self, originalGroups, classifiedGroups):
     self.originalGroups = originalGroups
     self.classifiedGroups = classifiedGroups
 
   def calculate(self):
-    currentLabels = {}
-    errors = 0
-    for i, value in enumerate(self.originalGroups):
-      if currentLabels.get(f'{value}') is None:
-        currentLabels[f'{value}'] = self.classifiedGroups[i]
-      elif currentLabels[f'{value}'] != self.classifiedGroups[i]:
-        errors += 1
-        currentLabels[f'{value}'] = self.classifiedGroups[i]
-    return (100 - (errors * 100 / len(self.originalGroups)))
+    return (metrics.rand_score(self.originalGroups, self.classifiedGroups) * 100)
